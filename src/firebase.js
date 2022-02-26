040429043b61e4b06e9b9fcfc898d29f921a892d
SABERGLOW <wali6985@gmail.com>
2022-02-26 19:43:06 +0100

    add firebaseConfig to firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+//... Copied from Firebase > Porject Settings > SDK setup and configuration > Config
+const firebaseConfig = {
+	apiKey: "AIzaSyCgdkUqmnav1f37M-9g1akaA-0mrqgyQpc",
+	authDomain: "facebook-clone-react-01110111.firebaseapp.com",
+	projectId: "facebook-clone-react-01110111",
+	storageBucket: "facebook-clone-react-01110111.appspot.com",
+	messagingSenderId: "521024147410",
+	appId: "1:521024147410:web:62db66acecc4072153820b",
+	measurementId: "G-3PZE4X6MJ3",
+};
