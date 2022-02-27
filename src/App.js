4e5108d628b36dd1738aaa92a01c2d019d0e29ee
SABERGLOW <wali6985@gmail.com>
2022-02-27 18:44:54 +0100

    add Header to app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Building a Facebook Clone...</h1>
+	return (
+		<div className='app'>
+			<h1>Building a Facebook Clone...</h1>
 
-      {/*... Header Component with Logo, Search, Homepage Icon etc ...*/  }
-      
+			{/*... Header Component with Logo, Search, Homepage Icon etc ...*/}
+			<Header />
 
-      {/*... App Body with Main Content ...*/  }
-        {/*... Side Bar ...*/  }
-        {/*... News Feed with Stories and Posts ...*/  }
-        {/*... Widgets ...*/  }
-    </div>
-  );
+			{/*... App Body with Main Content ...*/}
+			{/*... Side Bar ...*/}
+			{/*... News Feed with Stories and Posts ...*/}
+			{/*... Widgets ...*/}
+		</div>
+	);
 }
 
 export default App;
