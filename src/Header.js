0df7997b99847aae751c9c4c37f26304b57e5107
SABERGLOW <wali6985@gmail.com>
2022-02-27 18:44:29 +0100

    add right and middle header icons

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,44 @@
 import React from 'react'
 import './Header.css'
+import logo from './assets/logo512.png'
+import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Home';
+import FlagIcon from '@mui/icons-material/Flag';
+import SubscriptionsOutlinedIcon from '@mui/icons-material/SubscriptionsOutlined';
+import StorefrontOutlinedIcon from '@mui/icons-material/StorefrontOutlined';
+import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
 
 function Header() {
     return (
-        <div> className='Header'
+        <div className='header'>
+            <div className="header__left">
+                <img src={logo} alt='Facebook logo' />
+            </div>
+            <div className='header__input'>
+                <SearchIcon/>
+                <input type="text" />
+            </div>
+
+            <div className="header__middle">
+                <div className="header__option">
+                    <HomeIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    <FlagIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    <SubscriptionsOutlinedIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    <StorefrontOutlinedIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    <SupervisedUserCircleIcon fontSize="large"/>
+                </div>
+            </div>
+            <div className='header__right'>
+
+            </div>
 
         </div>
     )
