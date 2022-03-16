30264e3838aaf6c6a73fd655c99675744de9408a
SABERGLOW <wali6985@gmail.com>
2022-03-16 18:13:26 +0100

    import sidebar in app.js

--- Sidebar.js
+++ Sidebar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Sidebar.css"
+import SidebarRow from "./SidebarRow";
 
 function Sidebar() {
 	return ( 
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Sidebar from "../Sidebar";
 
 function App() {
 	return (
