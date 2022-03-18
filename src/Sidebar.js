2ad6f08eedf6bcbe645b1cfc8d3ddf22134ea71b
SABERGLOW <wali6985@gmail.com>
2022-03-18 13:17:25 +0100

    import icons, add them to sideBar div with titles

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,27 @@
 import React from "react";
 import "./Sidebar.css"
 import SidebarRow from "./SidebarRow";
+import profileAvatar from './assets/profileAvatar.jpg'
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
+import EmojiFlagsIcon from '@mui/icons-material/EmojiFlags';
+import PeopleIcon from '@mui/icons-material/People';
+import ChatIcon from '@mui/icons-material/Chat';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
+import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
+
 
 function Sidebar() {
 	return ( 
-        <div className="Sidebar">
-            <SidebarRow/>
+        <div className="sidebar">
+            <SidebarRow src={profileAvatar} title="Wali Ullah"/>
+            <SidebarRow Icon={LocalHospitalIcon} title="Covid-19 Information Center"/>
+            <SidebarRow Icon={EmojiFlagsIcon} title="Pages"/>
+            <SidebarRow Icon={PeopleIcon} title="Friends"/>
+            <SidebarRow Icon={ChatIcon} title="Messenger"/>
+            <SidebarRow Icon={StorefrontIcon} title="MarketPlace"/>
+            <SidebarRow Icon={VideoLibraryIcon} title="Videos"/>
+            <SidebarRow Icon={ExpandMoreOutlinedIcon} title="Marketplace"/>
         </div>
     )
 }
