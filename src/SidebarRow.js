1101215cabb5967b8a8d81347dcfb99eeb4d6281
SABERGLOW <wali6985@gmail.com>
2022-03-18 13:18:00 +0100

    pass src, Icon, title to SidebarRow as props

--- src/SidebarRow.js
+++ src/SidebarRow.js
@@ -1,9 +1,14 @@
+import { Avatar } from '@mui/material'
 import React from 'react'
 import "./SidebarRow.css"
 
-function SidebarRow() {
+function SidebarRow({src, Icon, title}) {
     return (
-        <div>SidebarRow</div>
+        <div className='sidebarRow'>
+            {src && <Avatar src={src} />}
+            {Icon && <Icon/>}
+            <h4>{title}</h4>
+        </div>
     )
 }
 
