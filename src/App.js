6ee45cd2a5fcaf8273a72f2e941623bece9898eb
SABERGLOW <wali6985@gmail.com>
2022-03-21 13:42:48 +0100

    add Feed.js and css, import in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import Feed from "./Feed";
 
 function App() {
 	return (
@@ -15,6 +16,7 @@ function App() {
 				{/*... Side Bar ...*/}
 				<Sidebar/>
 				{/*... News Feed with Stories and Posts ...*/}
+				<Feed/>
 				{/*... Widgets ...*/}
 			</div>
 
