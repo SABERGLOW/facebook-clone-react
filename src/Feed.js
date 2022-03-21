035033f90913e25f1c7cbba5397810d59aaffd9d
SABERGLOW <wali6985@gmail.com>
2022-03-21 13:48:55 +0100

    add StoryReel with boilerplate code

--- src/Feed.js
+++ src/Feed.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Feed.css'
+import './StoryReel.js'
 
 function Feed() {
     return (
         <div className='feed'>
             /* Story Reel */
+            <StoryReel/>
             /* Message Sender  */
         </div>
     )
---
+++ src/StoryReel.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './StoryReel.css'
+
+function StoryReel() {
+    return (
+        <div className='storyReel'>
+            /* Story */
+            /* Story */
+            /* Story */
+            /* Story */
+            /* Story */
+        </div>
+    )
+}
+
+export default StoryReel
