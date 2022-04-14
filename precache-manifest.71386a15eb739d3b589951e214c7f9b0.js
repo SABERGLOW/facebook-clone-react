306f0d3ff661fe8ba54c7f1799bb8dc60481e9ee
SABERGLOW <wali6985@gmail.com>
2022-04-14 21:31:12 +0000

    Deploying to gh-pages from @ SABERGLOW/facebook-clone-react@eb33b00e79ad3ec1688f9d51a56bfb49e1e2a5c7 🚀

---
+++ precache-manifest.71386a15eb739d3b589951e214c7f9b0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "5804b07924a05f3eb93e",
+    "url": "/facebook-clone-react/static/css/main.3507512c.chunk.css"
+  },
+  {
+    "revision": "5804b07924a05f3eb93e",
+    "url": "/facebook-clone-react/static/js/main.5804b079.chunk.js"
+  },
+  {
+    "revision": "5c7d1b126ae4d0bd9124",
+    "url": "/facebook-clone-react/static/js/1.5c7d1b12.chunk.js"
+  },
+  {
+    "revision": "8c37f80ff54541ab96cb",
+    "url": "/facebook-clone-react/static/js/2.8c37f80f.chunk.js"
+  },
+  {
+    "revision": "75fbd7cf7b891cdbc909",
+    "url": "/facebook-clone-react/static/js/runtime~main.75fbd7cf.js"
+  },
+  {
+    "revision": "f407c31b217aac6e0cd4171092d53a8c",
+    "url": "/facebook-clone-react/static/media/logo512.f407c31b.png"
+  },
+  {
+    "revision": "ec867a920e8c4414492e5844538a3425",
+    "url": "/facebook-clone-react/static/media/profileAvatar.ec867a92.jpg"
+  },
+  {
+    "revision": "baaa855a37bbc7806badc7543bcd88f9",
+    "url": "/facebook-clone-react/static/media/story1.baaa855a.jpg"
+  },
+  {
+    "revision": "e4ad9a9c5cc0c4132a9c3e95afb67e92",
+    "url": "/facebook-clone-react/static/media/story2.e4ad9a9c.jpg"
+  },
+  {
+    "revision": "248a704093c40a98a8c19d9b989080f3",
+    "url": "/facebook-clone-react/static/media/story3.248a7040.jpg"
+  },
+  {
+    "revision": "cfd11e38ad7e33f54c3eb00abd9d9c00",
+    "url": "/facebook-clone-react/static/media/story4.cfd11e38.jpg"
+  },
+  {
+    "revision": "ace12e7af49d61fa5ae5d8f40e07c8bf",
+    "url": "/facebook-clone-react/static/media/story5.ace12e7a.jpg"
+  },
+  {
+    "revision": "dc6a17c6dd62ca1f073e8cdea1f57f76",
+    "url": "/facebook-clone-react/static/media/logo512honeycomb.dc6a17c6.png"
+  },
+  {
+    "revision": "9db545c028cc5be9821895bda75a653b",
+    "url": "/facebook-clone-react/static/media/PhasebookTextLogo.9db545c0.png"
+  },
+  {
+    "revision": "a17978a501290f49b23a3348a9568d71",
+    "url": "/facebook-clone-react/index.html"
+  }
+];
