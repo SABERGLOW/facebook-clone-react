c76b8d326a893677aed1ed49188360c15de5d430
SABERGLOW <wali6985@gmail.com>
2022-04-14 23:19:04 +0200

    initialize firebase with firestore and auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore';
 
 //... Copied from Firebase > Porject Settings > SDK setup and configuration > Config
 const firebaseConfig = {
@@ -10,3 +12,13 @@ const firebaseConfig = {
 	appId: "1:521024147410:web:62db66acecc4072153820b",
 	measurementId: "G-3PZE4X6MJ3",
 };
+
+const firebaseApp = initializeApp(firebaseConfig); //... initialize firebaseApp with this config
+const db = getFirestore(firebaseApp); //... use firestore databse
+const auth = getAuth(); //... use firebase authentication
+const provider = new GoogleAuthProvider(); //... use Google as an authentication provider
+
+export {auth, provider, signInWithPopup	};
+export default db;
+
+// P.S: Putting the  Firebase settings in frontend is not a security risk. 
